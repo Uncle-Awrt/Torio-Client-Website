21ded4a9c843cbd34af980f6502aaaa53ff39cdb
kukentyan <kenichistute@outlook.com>
2026-07-09 23:42:36 +0900

    chore: update next.config.ts with production settings

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,12 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   devIndicators: false,
+  output: "export",
+  basePath: process.env.NODE_ENV === "production" ? "/Torio-Client-Website" : "",
+  assetPrefix: process.env.NODE_ENV === "production" ? "/Torio-Client-Website/" : "",
+  images: {
+    unoptimized: true,
+  },
 };
 
 export default nextConfig;
